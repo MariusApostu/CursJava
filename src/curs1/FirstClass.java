08399a6f72ee6a290e25c2d0c5ad885fb79cdda9
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-03-01 18:06:13 +0200

    first commit

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,18 @@
+package curs1;
+
+//comentariu pe o singura linie
+
+/*
+ * Multi line comment
+ */
+
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test");
+		
+	}
+
+}
