f629b21318e3ddc36b9c9b12c61f960490220c56
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-03-26 18:04:16 +0200

    curs8 final

---
+++ src/curs8/TestArrayExample3.java
@@ -0,0 +1,11 @@
+package curs8;
+
+public class TestArrayExample3 {
+
+	public static void main(String[] args) {
+		ArrayExample3 obj = new ArrayExample3();
+		//System.out.println(obj.generateNumber());
+		obj.printArray(obj.generateArray());
+	}
+
+}
