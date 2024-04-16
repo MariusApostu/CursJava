0542ccbe9aa5b9fcec91fec3ce8b9da8a815a0d2
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-04-16 18:04:40 +0300

    curs14 final

---
+++ src/curs14/TestPropertiesFile.java
@@ -0,0 +1,30 @@
+package curs14;
+
+public class TestPropertiesFile {
+
+	public static void main(String[] args) {
+		
+		PropertiesFileProcessor obj = new PropertiesFileProcessor();
+		
+		obj.writePropertiesFile();
+		
+		obj.readPropertiesFile("hostname");
+		obj.readPropertiesFile("email");
+		obj.readPropertiesFile("user");
+		obj.readPropertiesFile("url");
+		obj.readPropertiesFile("pass");
+		
+		System.out.println("----------------------------------------------------");
+		
+		obj.updatePropertiesFile("user", "Oana");
+		obj.readPropertiesFile("user");
+		
+		System.out.println("----------------------------------------------------");
+		
+		obj.deleteFromPropertiesFile("user");
+		obj.readPropertiesFile("user");
+		
+
+	}
+
+}
