0f9a8f9a7faf5ca20712d9847132258406b84044
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-04-19 16:49:14 +0300

    curs15 intermediar

---
+++ src/curs15/TestJsonProcessor.java
@@ -0,0 +1,14 @@
+package curs15;
+
+public class TestJsonProcessor {
+
+	public static void main(String[] args) {
+		
+		JsonFileProcessor obj = new JsonFileProcessor();
+		
+		obj.writeJsonFile();
+		obj.readJsonFile("email");
+		
+	}
+
+}
