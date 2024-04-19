60481077fb94abb9891af4cc42ea3825ac7ebf42
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-04-19 15:33:27 +0300

    curs15 final

--- src/curs14/JsonFileProcessor.java
+++ src/curs14/JsonFileProcessor.java
@@ -1,5 +1,14 @@
 package curs14;
 
+import org.json.simple.JSONObject;
+
 public class JsonFileProcessor {
+	
+	
+	public void writeJsonfile() {
+		
+		JSONObject obj = new JSONObject();
+		
+	}
 
 }
